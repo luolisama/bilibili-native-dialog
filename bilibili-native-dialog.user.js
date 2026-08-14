ad4cc70ddfd958fcfc02cc36cb621ed36ecbecf9
落漓sama <54465497+luolisama@users.noreply.github.com>
2026-08-15 03:33:28 +0800

    chore: bump userscript version to 0.5.12

--- bilibili-native-dialog.user.js
+++ bilibili-native-dialog.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         bilibili类原生查看对话
 // @namespace    https://github.com/luolisama/bilibili-native-dialog
-// @version      0.5.11
+// @version      0.5.12
 // @author       luolisama
 // @downloadURL  https://github.com/luolisama/bilibili-native-dialog/raw/refs/heads/main/bilibili-native-dialog.user.js?download=1
 // @updateURL    https://github.com/luolisama/bilibili-native-dialog/raw/refs/heads/main/bilibili-native-dialog.user.js?download=1
